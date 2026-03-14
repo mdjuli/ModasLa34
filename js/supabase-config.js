01e090042565fd8235d935d851bf018efcfb6abe
mdjuli <jgarcia616@unab.edu.co>
2026-03-13 20:29:06 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://frvcpaymckwyxwcvmugl.supabase.co”></script';
+const SUPABASE_URL = 'https://frvcpaymckwyxwcvmugl.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydmNwYXltY2t3eXh3Y3ZtdWdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNTU0MjgsImV4cCI6MjA4ODkzMTQyOH0.9sT1fV333AunNH0X__tXuIaeXM3Qrd_jp12ljFpOWzM';
 
 console.log('✅ Configuración de Supabase cargada');
